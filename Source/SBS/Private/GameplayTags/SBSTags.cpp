4fd76dc9764c2b8ebfc332108824b86274dfb192
HeheKrazy <AdamSabo2750@Gmail.com>
2025-10-23 22:45:27 -0400

    Native Gameplay Tags

---
+++ Source/SBS/Private/GameplayTags/SBSTags.cpp
@@ -0,0 +1,13 @@
+// Copyright HeheKrazy
+
+
+#include "GameplayTags/SBSTags.h"
+
+
+namespace SBSTags
+{
+	namespace SBSAbilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "SBSTags.SBSAbilities.Primary", "Primary ability tag for SBS");
+	}
+}
---
+++ Source/SBS/Public/GameplayTags/SBSTags.h
@@ -0,0 +1,18 @@
+// Copyright HeheKrazy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+/**
+ * 
+ */
+
+namespace SBSTags
+{
+	namespace SBSAbilities
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Primary);
+	}
+}
